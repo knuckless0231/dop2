5ec17c52a59fb141e44c710f8c60bfb410f6ba08
Kostya Kokhanov <knuckostya@gmail.com>
2022-10-28 19:32:26 +0300

    ddddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Components/Button";
+
+
+type ShowType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [show, setShow] = useState<ShowType[]>([])
+
+
+    const showApp = () => {
+
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setShow(json))
+
+    }
+
+    const clean = ()=>{
+        setShow([])
+    }
+
+    // const filter = ()=>{
+    //
+    //
+    //
+    // }
+
+    return (
+        <div className="App">
+            <Button name={'Show me'} callback={showApp}/>
+            <Button name={'Hide Me'} callback={clean}/>
+            {/*<Button name={'Filter'} callback={filter}/>*/}
+
+            <ul>
+                {show.map((e) => {
+                    return (
+                        <li key={e.id}>
+                            <span>{e.userId}</span>
+                            <span>{e.id}</span>
+                            <span>{e.title}</span>
+                            <span> {`${e.completed}`}</span>
+                        </li>
+                    );
+                })}
+
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
